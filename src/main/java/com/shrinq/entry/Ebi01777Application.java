f44d290f2759ec1e9de8f34ac21bb9ba5408c507
Bryan Laryea <bry_lar@yahoo.com>
2021-03-20 03:01:56 +0000

    first commit

---
+++ src/main/java/com/shrinq/entry/Ebi01777Application.java
@@ -0,0 +1,13 @@
+package com.shrinq.entry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ebi01777Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ebi01777Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/shrinq/entry/Ebi01777ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrinq.entry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ebi01777ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
